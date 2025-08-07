abb539fff5a83961eb665fe81b0fea391e5ce2f4
Prajwal M <prajwalm377@gmail.com>
2025-08-07 17:23:44 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './Pages/Home';
-// import Login from './Components/Auth/Login';
-// import Register from './Components/Auth/Register';
-// import UserDetails from './Components/Auth/UserDetails';
-// import AdminDashboard from './Pages/AdminDashboard';
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/userDetails" element={<UserDetails />} />
-//         <Route path="/home" element={<Home />} />
-//         <Route path="/admin" element={<AdminDashboard />} />
-
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-
-// export default App;
-
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
